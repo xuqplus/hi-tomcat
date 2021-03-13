6ccfc3e444e3efd3d094d55353fd016a14b88737
Qunqun Xu <xuqunqun@live.cn>
2021-03-13 12:56:03 +0800

    Initialize

---
+++ src/main/java/com/github/xuqplus/hi/tomcat/HiTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.github.xuqplus.hi.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/xuqplus/hi/tomcat/HiTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xuqplus.hi.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
